4aa245282a5e713d231d97dee237181aa990ea9f
sukie <1170062792@qq.com>
2018-06-14 15:55:55 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    text: '你好呀，胖子',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
